10510c200bc83acfacc4368d22d117094612a549
Orlando Malo <orlandomalo0@gmail.com>
2023-06-26 21:08:07 +0200

    added bookings page

--- src/Client/Shared/IReportService.cs
+++ src/Client/Shared/IReportService.cs
@@ -1,8 +1,10 @@
 ﻿using FSH.BlazorWebAssembly.Client.ViewModels;
+using static FSH.BlazorWebAssembly.Client.Pages.Identity.Roles.GenerateReportBookings;
 
 namespace FSH.BlazorWebAssembly.Client.Shared;
 
 public interface IReportService<T> where T : class
 {
     Task GenerateReport(string endpoint, List<T> reportData);
+    Task GenerateReportForBookings(BookingReportDto report);
 }
